059fff1c9a94a9fe691f77a5150e45dd5abd435c
sathishkumar <satish102719@gmail.com>
2024-07-23 16:45:13 +0530

    first commit

---
+++ app/src/main/java/com/example/threadpoolexecutor/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.threadpoolexecutor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.SynchronousQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val threadPool = ThreadPoolExecutor(
+            2,
+            10,
+            2000,
+            TimeUnit.MILLISECONDS,
+            SynchronousQueue()
+        )
+
+        threadPool.execute{
+            for(i in 1..100000){
+                println("Task 1 is running on ${Thread.currentThread().name}")
+            }
+            println("Task 1 ThreadName123: ${Thread.currentThread().name}")
+        }
+
+        threadPool.execute{
+            for(i in 1..100000){
+                println("Task 2 is running on ${Thread.currentThread().name}")
+            }
+            println("Task 2 ThreadName123: ${Thread.currentThread().name}")
+        }
+        threadPool.execute{
+            for(i in 1..100000){
+                println("Task 3 is running on ${Thread.currentThread().name}")
+            }
+            println("Task 3 ThreadName123: ${Thread.currentThread().name}")
+        }
+        threadPool.execute{
+            for(i in 1..100000){
+                println("Task 4 is running on ${Thread.currentThread().name}")
+            }
+            println("Task 4 ThreadName123: ${Thread.currentThread().name}")
+        }
+        threadPool.execute{
+            for(i in 1..100000){
+                println("Task 5 is running on ${Thread.currentThread().name}")
+            }
+            println("Task 5 ThreadName123: ${Thread.currentThread().name}")
+        }
+        threadPool.execute{
+            for(i in 1..100000){
+                println("Task 5 is running on ${Thread.currentThread().name}")
+            }
+            println("Task 6 ThreadName123: ${Thread.currentThread().name}")
+        }
+        threadPool.execute{
+            for(i in 1..100000){
+                println("Task 5 is running on ${Thread.currentThread().name}")
+            }
+            println("Task 7 ThreadName123: ${Thread.currentThread().name}")
+        }
+        threadPool.execute{
+            for(i in 1..100000){
+                println("Task 8 is running on ${Thread.currentThread().name}")
+            }
+            println("Task 8 ThreadName123: ${Thread.currentThread().name}")
+        }
+        threadPool.execute{
+            for(i in 1..100000){
+                println("Task 8 is running on ${Thread.currentThread().name}")
+            }
+            println("Task 9 ThreadName123: ${Thread.currentThread().name}")
+        }
+    }
+}
